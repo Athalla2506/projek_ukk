899f05eaee2435155c378df77e6213e392ad551c
Atha <athallafadly@gmail.com>
2025-06-03 13:02:35 +0700

    modif tampilan dikit

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+<div class="flex aspect-square size-14 items-center justify-center rounded-full bg-accent-content text-accent-foreground">
+    <img src="{{ asset('image/sija.png') }}" alt="Logo" class="rounded-full">
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold">PKL SIJA</span>
 </div>
